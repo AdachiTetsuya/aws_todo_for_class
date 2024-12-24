64684cae7424b8d330ec88dec6403081f87f4d28
AdachiTetsuya <1019born@gmail.com>
2024-12-24 12:28:55 +0900

    API通信の追加

---
+++ src/api/config.js
@@ -0,0 +1,6 @@
+export const ApiUrls = {
+    getTodo:
+        "https://lgw908xojh.execute-api.ap-northeast-1.amazonaws.com/default/GetTodo",
+    manageTodo:
+        "https://kdlyuwfhzh.execute-api.ap-northeast-1.amazonaws.com/default/ManageTodo",
+};
